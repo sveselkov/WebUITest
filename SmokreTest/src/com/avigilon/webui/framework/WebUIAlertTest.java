42d2e96c4f1eade009cc6afb961ba8ec68e232f2
Serge <spbsves@gmail.com>
2015-12-25 18:00:49 -0500

    Update

--- SmokreTest/src/com/avigilon/webui/framework/WebUIAlertTest.java
+++ SmokreTest/src/com/avigilon/webui/framework/WebUIAlertTest.java
@@ -5,7 +5,12 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Admin on 12/25/2015.
@@ -16,7 +21,10 @@ public class WebUIAlertTest {
     public void webUIGoogleTest(){
         final String googleURL = "www.google.com";
 
-        WebDriver driver = new FirefoxDriver();
+        FirefoxProfile profile = new FirefoxProfile();
+        FirefoxBinary binary = new FirefoxBinary(new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"));
+        WebDriver driver = new FirefoxDriver(binary,profile);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get(googleURL);
 
